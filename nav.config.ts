19015ad281677df5ac1e5e16dadb0e219a687b92
jiuLiang <chinalzge@gmail.com>
2024-08-16 18:01:32 +0800

    Update nav.config.ts

--- nav.config.ts
+++ nav.config.ts
@@ -3,8 +3,8 @@ import { IConfig } from './src/types'
 const c: IConfig = {
   // [Mondatory], Please replace following Github url with your own Github address which you forked in.
   // [必填], 请填写您的仓库地址， 地址最后不要带 /
- // gitRepoUrl: 'https://github.com/liang1024/nav',
-gitRepoUrl: 'https://liang1024.github.io/nav',
+ gitRepoUrl: 'https://github.com/liang1024/nav',
+  //gitRepoUrl: 'https://liang1024.github.io/nav',
   // 部署平台
   // 可选 Github | Gitee，如果是 Gitee 上面一定要填写 Gitee 的仓库地址
   // Gitee 官方图片新增防盗链，不要使用上传图片功能，自己填入图片地址
